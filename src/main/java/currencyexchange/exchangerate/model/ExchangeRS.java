47df4034f85b77879be8e45b70c9ae89e1a03527
asdasdasdasdasdasd1 <abromero2102@gmail.com>
2021-03-18 15:27:06 -0500

    First commit

---
+++ src/main/java/currencyexchange/exchangerate/model/ExchangeRS.java
@@ -0,0 +1,17 @@
+package currencyexchange.exchangerate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExchangeRS {
+	
+	private Integer idCurrency;
+	private float currency;
+
+}
